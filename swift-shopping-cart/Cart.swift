eac96f003aa851df7b501012ed586924ad59f4e0
jicaey <michaelyoung.dev@gmail.com>
2016-09-28 18:20:45 -0400

    Lab Complete

--- swift-shopping-cart/Cart.swift
+++ swift-shopping-cart/Cart.swift
@@ -7,6 +7,62 @@
 //
 
 class Cart {
+    var items = [Item]()
+
+    func totalPriceInCents() -> Int {
+        var totalCost = Int()
+        for item in items{
+            let itemCost = item.priceInCents
+            totalCost += itemCost
+        }
+        return totalCost
+    }
     
+    func add(item: Item) {
+        items.append(item)
+    }
     
+    func remove(item: Item) {
+        for (index,value) in items.enumerated() {
+            if item == value {
+                items.remove(at: index)
+            }
+        }
+    }
+    
+    func items(withName name: String) -> [Item] {
+        var final = [Item]()
+
+        for item in items {
+            if name == item.name {
+                final.append(item)
+            }
+        }
+        
+        return final
+    }
+    
+    func items(withMinPrice price: Int) -> [Item] {
+        var final = [Item]()
+        for item in items {
+            if price <= item.priceInCents {
+                final.append(item)
+            }
+        }
+        return final
+    }
+    
+    func items(withMaxPrice price: Int) -> [Item] {
+        var final = [Item]()
+        for item in items {
+            if price >= item.priceInCents {
+                final.append(item)
+            }
+        }
+        return final
+    }
 }
+
+
+
+
